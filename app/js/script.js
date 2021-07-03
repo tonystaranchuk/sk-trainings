4d2ed912b93f4c1b74241bc12c01ae7c8e8c91bf
Tony <tonystaranchuk@gmail.com>
2021-07-03 18:47:26 +0300

    training13

---
+++ app/js/script.js
@@ -0,0 +1,19 @@
+let i = 0;
+console.log(typeof (i));
+
+if(i === 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+console.log((i===0))
+
+console.log((i==0)?(true):false);
+
+let a = 6; b = 3;
+
+console.log(a>b?true:false);
+
+let str = prompt("Input your digit",0);
+alert(str);
